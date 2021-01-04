3bf954f0ebde47df9ea40b76828ba216b8d58288
“guoyaweiCode” <gyw435945337>
2021-01-05 01:10:03 +0800

    feat(demo210104-shiro):shiro 认证授权

---
+++ demo210104-shiro/src/main/java/com/guoyw/dem210104/shiro/Dem210104ShiroApplication.java
@@ -0,0 +1,13 @@
+package com.guoyw.dem210104.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dem210104ShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dem210104ShiroApplication.class, args);
+    }
+
+}
---
+++ demo210104-shiro/src/test/java/com/guoyw/dem210104/shiro/Dem210104ShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoyw.dem210104.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dem210104ShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
